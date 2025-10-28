46dc4ce8b8e0f4723785566c36c60295eaaa0cc4
Siddhartha-jangra <jangrasidhu1876@gmail.com>
2025-10-28 10:33:31 +0530

    adding questions and when to use parameters

--- chapter-2-learncpp/ParametersAndReturnValues2.5/whenToUseParameters.cpp
+++ chapter-2-learncpp/ParametersAndReturnValues2.5/whenToUseParameters.cpp
@@ -1,3 +1,8 @@
+/*these code block of code shows us when to not use a parameter.
+  here it can be dierictly done by writing:
+  cout << dosomething;
+  where a variable in defined in the function and not as a parameter */
+
 #include <iostream>
 
 using namespace std;
