9cc4485962db617693f717a68970b618844dad66
Siddhartha-jangra <jangrasidhu1876@gmail.com>
2026-01-21 19:45:02 +0530

    erase and insert function

--- dsaSeries/vectorsIntroduction/vectorMemberFunction.cpp
+++ dsaSeries/vectorsIntroduction/vectorMemberFunction.cpp
@@ -4,40 +4,49 @@
 3. pop_back() : to delete the last data of the vector
 3. front : to go to the front of the vector
 4. back : to go to the back of the vector
-5. at() : works the same way as the vec_name(i) as in vec.at(i)*/
-
+5. at() : works the same way as the vec_name(i) as in vec.at(i)
+6. insert() : to insert a value at any index
+7. erase() : to erase a valur at any index*/
 
 #include <iostream>
 #include <vector> // std library for the vectors
+using namespace std;
 
 int main()
-{
-    std::vector<int> vec {1,2,3,4,5};
+{   
+    vector<int> vec {1,2,3,4,5};
     //to check the size 
-    std::cout << vec.size() << '\n';
+    cout << vec.size() << '\n';
 
     //for each loop
     for(int i : vec)
     {
-        std::cout << i << '\n';
+        cout << i << '\n';
     }
 
     //to put value in the back
     vec.push_back(6);
     for(int i : vec)
     {
-        std::cout << i << '\n';
+        cout << i << '\n';
     }
 
     //prints the valuse in the front
-    std::cout << vec.front() << '\n';
+    cout << vec.front() << '\n';
 
     //print the value iof the end
-    std::cout << vec.back() << '\n';
+    cout << vec.back() << '\n';
 
     //goes at the location mentioned.
-    std::cout << vec.at(3) << '\n';
+    cout << vec.at(3) << '\n';
 
+    //insert() : to put any valur at any position
+    vec.insert(vec.begin() + 2, 200);
+    cout << vec[2] << '\n';
+
+    // to erase
+    vec.erase(vec.begin() + 2);
+    cout << vec[2] << '\n';
 
     
 
